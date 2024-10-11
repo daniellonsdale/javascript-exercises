13954b8a9a62b5938bd4228728033ba204c83938
daniellonsdale <danielllonsdale@gmail.com>
2024-10-12 00:31:22 +0900

    Create basic string reversal func

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let output = '';
+    for (let i = 0; i < input.length; i++){
+        output += input.at(input.length - i);
+    }
+    return output;
 };
 
 // Do not edit below this line
