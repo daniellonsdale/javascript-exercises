a297197bdff02cc7b5270dd85048eadfa3f7218d
daniellonsdale <danielllonsdale@gmail.com>
2024-10-14 13:49:35 +0900

    Resolve rounding problem

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,14 +1,14 @@
 const convertToCelsius = function(fInput) {
   let converted;
   converted = ((fInput - 32)*(5/9));
-  let rounded = Math.round((converted * 10)/10);
+  let rounded = Math.round(converted * 10) / 10;
   return rounded;
 };
 
 const convertToFahrenheit = function(cInput) {
   let converted;
   converted = (cInput * (9/5) + 32);
-  let rounded = Math.round((converted * 10)/10);
+  let rounded = Math.round(converted * 10) / 10;
   return rounded;
 };
 
