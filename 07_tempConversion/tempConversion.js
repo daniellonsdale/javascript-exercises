e91b1614408be01a12e7687d18c2c5219a1c78f0
daniellonsdale <danielllonsdale@gmail.com>
2024-10-14 13:44:11 +0900

    Write initial convertToCelsius func

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fInput) {
+  let converted;
+  converted = ((fInput - 32)*(5/9));
+  let rounded = Math.round((converted * 10)/10);
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cInput) {
 };
 
 // Do not edit below this line
