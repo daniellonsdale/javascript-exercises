f98cc1e9c3a10f0ebf28edbf960192505195bbb5
daniellonsdale <danielllonsdale@gmail.com>
2024-10-14 12:54:05 +0900

    Create function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(firstInput, SecondInput) {
+    //Init return var
+    //Set smaller and larger values
+    //Validate input args
+    //Loop from firstInput to secondInput and summate inclusively
 
-};
+    let sum = 0;
+    let largerValue;
+    let smallerValue;
 
+    if(firstInput > SecondInput){
+        largerValue = firstInput;
+        smallerValue = SecondInput;
+    }else if (SecondInput => firstInput){
+        largerValue = SecondInput;
+        smallerValue = firstInput;
+    }
+
+    if (firstInput >= 0 && Number.isInteger(firstInput) && SecondInput >= 0 && Number.isInteger(firstInput)){
+        for (let i = firstInput; i <= SecondInput; i++) {
+            sum += i;
+        }
+        return sum;
+    }else{
+        return 'ERROR';
+    }
+}
 // Do not edit below this line
 module.exports = sumAll;
