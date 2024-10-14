881e6faa3fc43bbaf2732cda7d9fc40dc0b38dab
daniellonsdale <danielllonsdale@gmail.com>
2024-10-14 13:38:19 +0900

    Complete tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
     //Set year argument
     //Test if true for all conditions
     //Return true if yes, false if no
 
-    
+    if (year % 4 === 0){
+        if(year % 100 === 0 && year % 400 === 0){
+            return true;
+        }else if (year % 100 === 0){
+            return false;
+        }else{
+            return true;
+        }
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
