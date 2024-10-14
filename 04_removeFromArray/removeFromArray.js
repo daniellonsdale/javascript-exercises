5e6c63919149a5702b27fe1a9031527bf8665c53
daniellonsdale <danielllonsdale@gmail.com>
2024-10-14 12:29:11 +0900

    Create initial solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...rem) {
+    let result = [];
+
+    for (const i of inputArray){
+        let toRemove = false;
+        for (const j of rem){
+            if(i === j){
+                toRemove = true;
+            }
+        }
+        if (toRemove){
+            break;
+        }
+        else{
+            result.push(i);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
