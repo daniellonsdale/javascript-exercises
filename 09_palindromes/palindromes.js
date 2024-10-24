d7bbd2a72c4b6b86aa053bda45e51fad5a98cc05
daniellonsdale <danielllonsdale@gmail.com>
2024-10-25 00:57:34 +0900

    Complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (inputStr) {
+    inputStr = inputStr.toLowerCase();
+    let noNonLetters = inputStr.replaceAll(/\W/ig, '');
+    let verifiedStr = noNonLetters.replaceAll(' ', '');
+    let reversed = "";
+    for (let i = verifiedStr.length; i > 0; i--){
+        reversed += verifiedStr.charAt(i - 1);
+    }
+    console.log(reversed);
+    if (reversed === verifiedStr){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
