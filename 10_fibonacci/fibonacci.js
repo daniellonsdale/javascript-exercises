51390969f89db9b11e6607a4c326ae3f1cf39811
daniellonsdale <danielllonsdale@gmail.com>
2024-10-27 16:08:49 +0900

    Complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let arr = [];
+    if(num < 0){
+        return "OOPS";
+    }
+    if(num == 0){
+        return 0;
+    }
+    for (let i = 0; i < num; i++){
+        if(i === 0 || i === 1){
+            arr[i] = 1;
+        }else{
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+    }
+    return arr[num - 1];
 };
 
 // Do not edit below this line
