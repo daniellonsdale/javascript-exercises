ae1cea896fb61de6d23c2991ae845c2025df950f
daniellonsdale <danielllonsdale@gmail.com>
2024-10-27 16:40:20 +0900

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const CURRENTYEAR = new Date().getFullYear();
+    let oldest = people[0];
+    let ages = [];
+    let curOldestAge  = 0;
 
+    for (i in people){
+        if(Object.hasOwn(people[i], 'yearOfDeath')){
+            ages[i] = people[i].yearOfDeath - people[i].yearOfBirth;
+        }
+        else{
+            ages[i] = CURRENTYEAR - people[i].yearOfBirth;
+        }
+
+        if (ages[i] > curOldestAge){
+            oldest = people[i];
+            curOldestAge = ages[i];
+        }
+    }
+
+
+    return oldest;
 };
 
 // Do not edit below this line
